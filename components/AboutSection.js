8b175a4e6f14d4eda1ecbc68fba5ff86f8fa4f58
barrystone <barrystone3183@gmail.com>
2021-05-23 23:08:47 +0800

    [update] AboutSection layout

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -12,7 +12,7 @@ const AboutSection = () => {
       </div>
       <div className="section-about__textbox-2">
         <h3 className="heading-tertiary paragraph-big--4" id="skill">
-          <span>技能</span>
+          <span>技能包</span>
         </h3>
       </div>
       <section className="section-about__skillsetbox">
