05aa3606bfeeb2d0ff4c04ee233832d4a350f729
barrystone <barrystone3183@gmail.com>
2021-05-24 14:26:14 +0800

    [update] developing info on developing page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const about = () => {
-  return <div style={{ color: '#fff', fontSize: '40rem' }}>about me</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        color: '#fff',
+        fontSize: '10rem',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        height: '100vh',
+        width: '100%'
+      }}
+    >
+      <div style={{}}>開發中...</div>
+      <p />
+      <div style={{}}>Developing</div>
+    </div>
+  );
 };
 
 export default about;
--- pages/note.js
+++ pages/note.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const note = () => {
-  return <div style={{ color: '#fff', fontSize: '40rem' }}>note</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        color: '#fff',
+        fontSize: '10rem',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        height: '100vh',
+        width: '100%'
+      }}
+    >
+      <div style={{}}>開發中...</div>
+      <p />
+      <div style={{}}>Developing</div>
+    </div>
+  );
 };
 
 export default note;
