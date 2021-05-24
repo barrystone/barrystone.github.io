31b0f3c4b6d01d07d3cca0c80eed00c1464823b4
barrystone <barrystone3183@gmail.com>
2021-05-24 14:36:25 +0800

    [update] developing page basic style

--- pages/note.js
+++ pages/note.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const note = () => {
   return (
@@ -17,6 +18,24 @@ const note = () => {
       <div style={{}}>開發中...</div>
       <p />
       <div style={{}}>Developing</div>
+      <Link href="/" as={process.env.BACKEND_URL}>
+        <a
+          style={{
+            color: 'black',
+            textDecoration: 'none',
+            position: 'absolute',
+            top: '5rem',
+            left: '10rem',
+            fontSize: '5rem',
+            backgroundColor: 'white',
+            padding: '1rem 2rem',
+            borderRadius: '15px',
+            fontWeight: 'bold'
+          }}
+        >
+          首頁
+        </a>
+      </Link>
     </div>
   );
 };
