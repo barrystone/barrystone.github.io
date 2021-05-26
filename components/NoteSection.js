468eb5579f728c36f90692835ca70885c847013c
barrystone <barrystone3183@gmail.com>
2021-05-26 15:40:11 +0800

    [update] notetree style on NoteSection

--- components/NoteSection.js
+++ components/NoteSection.js
@@ -19,25 +19,54 @@ const NoteSection = () => {
     }
   };
 
+  const listNotes = 6;
+  // setTimeout(() => console.log(Object.values(notes)), 2000);
+
   return (
     <section className="section section-note">
       <div className="section-note__container">
         {Object.keys(notes)
-          .splice(0, 8)
+          .splice(0, 10)
           .map((e, idx) => (
             <section className="section-note__topicbox" key={idx}>
               <div className="notetree">
-                <div className="notetree__title">
+                <div
+                  className={`notetree__title ${
+                    e.length > 12 ? 'notetree__title-font--small' : ''
+                  }`}
+                >
                   <p>{e}</p>
                 </div>
                 <ul key={idx}>
-                  {notes[e].map((f, index) => (
-                    <li key={index}>
+                  {notes[e].splice(0, listNotes).map((f, index) => (
+                    <li
+                      key={index}
+                      className={
+                        Object.keys(f).toString().length >= 20
+                          ? 'notetree__li-font--small'
+                          : // console.log('big')
+                            ''
+                      }
+                    >
                       <a href={Object.values(f)} target="_blank">
                         {Object.keys(f)}
                       </a>
                     </li>
                   ))}
+                  {notes[e].length > 0 ? (
+                    <li
+                      style={{
+                        listStyle: 'none',
+                        letterSpacing: '.3rem',
+                        fontSize: '1.8rem',
+                        marginTop: '-1.5rem'
+                      }}
+                    >
+                      ...
+                    </li>
+                  ) : (
+                    ''
+                  )}
                 </ul>
               </div>
             </section>
