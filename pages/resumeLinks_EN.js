0e81bce19b0b7324f77dae16bd127dbcb1bf1c02
barrystone <barrystone3183@gmail.com>
2022-12-02 03:22:40 +0800

    [feat] create EN page of resumeLinks (resumeLinks_EN)

--- pages/resumeLinks_EN.js
+++ pages/resumeLinks_EN.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 
-const resumeLinks = ({ projectsData, achievementData }) => {
+const resumeLinks_EN = ({ projectsData, achievementData }) => {
   const projectFields = projectsData[0];
   const projects = projectsData.slice(1);
   const achievements = achievementData.slice(1);
@@ -9,35 +9,36 @@ const resumeLinks = ({ projectsData, achievementData }) => {
   return (
     <div id="resumelinkspage">
       <Head>
-        <title>石在元的專案&成果經歷</title>
+        <title>Zai-Yuan's Projects & Achievements</title>
       </Head>
       <div className="resumelinks">
         <div className="github">
-          <span>其他專案:</span>
+          <span>Other projects</span>
           <a href="https://github.com/barrystone" target="_blank">
             Github
           </a>
         </div>
         <section>
-          <h2>專案</h2>
+          <h2>Projects</h2>
           <ul>
             {projects.map((e) => (
               <li>
-                {/* project name */}
-                <span>{e[0]}</span>
+                {/* project name  EN (0=>6) */}
+                <span>{e[6]}</span>
                 <br />
                 {/* project fields */}
                 {e.slice(1).map((field, index) =>
-                  field ? (
+                  field && index < 5 ? (
                     <div className="projectfield">
                       {/* project field */}
-                      {/* For my custom field in google sheet (FOR EN MODE)*/}
-                      <span>{projectFields[index + 1].split(',')[0]}:</span>
+                      <span>{projectFields[index + 1].split(',')[1]}:</span>
+
                       <br />
                       {/* For my custom field in google sheet (pdf download field)*/}
                       {index === 2 ? (
                         <a href={field.split(',')[0]} target="_blank">
-                          {field.split(',')[1]}
+                          {/*  EN (0=>6) */}
+                          {field.split(',')[2]}
                         </a>
                       ) : (
                         <a href={field} target="_blank">
@@ -52,11 +53,13 @@ const resumeLinks = ({ projectsData, achievementData }) => {
           </ul>
         </section>
         <section>
-          <h2>成果經歷</h2>
+          <h2>Achievements</h2>
           <ul>
             {achievements.map((e) => (
               <li>
-                <span>{e[0]}:</span>
+                {/* EN (0=>2) */}
+                <span>{e[2]}:</span>
+                <br />
                 <br />
                 <a href={e[1]} target="_blank">
                   {e[1]}
@@ -70,13 +73,13 @@ const resumeLinks = ({ projectsData, achievementData }) => {
   );
 };
 
-export default resumeLinks;
+export default resumeLinks_EN;
 
 export async function getStaticProps() {
   // Because of "Error occurred prerendering page" github action deploying error, environmant variable might working in dev mode only(but why?).
   // const range = `A:E`;
   const sheetProjectApi =
-    'https://sheets.googleapis.com/v4/spreadsheets/10q0h4IoGn-PY1K62Q3H4T15mMBhPshQvBc_AqIs-zL4/values/A:E?key=AIzaSyDXtHX8ByZFCQJPta9HOFZlJOcOhlf8b5Q';
+    'https://sheets.googleapis.com/v4/spreadsheets/10q0h4IoGn-PY1K62Q3H4T15mMBhPshQvBc_AqIs-zL4/values/A:G?key=AIzaSyDXtHX8ByZFCQJPta9HOFZlJOcOhlf8b5Q';
   // const sheetProjectApi = `${process.env.NEXT_PUBLIC_GOOGLE_SHEET_API_BASE}/${process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID_PROJECT}/values/${range}?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`;
   const { values: projectValues } = await fetch(sheetProjectApi).then((res) =>
     res.json()
@@ -84,7 +87,7 @@ export async function getStaticProps() {
   const projectsData = projectValues;
 
   const sheetAchievementApi =
-    'https://sheets.googleapis.com/v4/spreadsheets/1E3TcRRSEYTAZ4w-io-uDvcDAifxfit8klSs6gSVfiY0/values/A:E?key=AIzaSyDXtHX8ByZFCQJPta9HOFZlJOcOhlf8b5Q';
+    'https://sheets.googleapis.com/v4/spreadsheets/1E3TcRRSEYTAZ4w-io-uDvcDAifxfit8klSs6gSVfiY0/values/A:G?key=AIzaSyDXtHX8ByZFCQJPta9HOFZlJOcOhlf8b5Q';
   // const sheetAchievementApi = `${process.env.NEXT_PUBLIC_GOOGLE_SHEET_API_BASE}/${process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID_ACHIEVEMENT}/values/${range}?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`;
   const { values: achievementValues } = await fetch(sheetAchievementApi).then(
     (res) => res.json()
