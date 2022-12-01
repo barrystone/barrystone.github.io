51816fe3ac2bfc4e1b3763eaadae8daf2c75fd5d
barrystone <barrystone3183@gmail.com>
2022-12-02 02:07:36 +0800

    [fix] Error occurred prerendering page

--- pages/resumeLinks.js
+++ pages/resumeLinks.js
@@ -72,14 +72,18 @@ const resumelinks = ({ projectsData, achievementData }) => {
 export default resumelinks;
 
 export async function getStaticProps() {
-  const range = `A:E`;
-  const sheetProjectApi = `${process.env.NEXT_PUBLIC_GOOGLE_SHEET_API_BASE}/${process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID_PROJECT}/values/${range}?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`;
+  // const range = `A:E`;
+  const sheetProjectApi =
+    'https://sheets.googleapis.com/v4/spreadsheets/10q0h4IoGn-PY1K62Q3H4T15mMBhPshQvBc_AqIs-zL4/values/A:E?key=AIzaSyDXtHX8ByZFCQJPta9HOFZlJOcOhlf8b5Q';
+  // const sheetProjectApi = `${process.env.NEXT_PUBLIC_GOOGLE_SHEET_API_BASE}/${process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID_PROJECT}/values/${range}?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`;
   const { values: projectValues } = await fetch(sheetProjectApi).then((res) =>
     res.json()
   );
   const projectsData = projectValues;
 
-  const sheetAchievementApi = `${process.env.NEXT_PUBLIC_GOOGLE_SHEET_API_BASE}/${process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID_ACHIEVEMENT}/values/${range}?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`;
+  const sheetAchievementApi =
+    'https://sheets.googleapis.com/v4/spreadsheets/1E3TcRRSEYTAZ4w-io-uDvcDAifxfit8klSs6gSVfiY0/values/A:E?key=AIzaSyDXtHX8ByZFCQJPta9HOFZlJOcOhlf8b5Q';
+  // const sheetAchievementApi = `${process.env.NEXT_PUBLIC_GOOGLE_SHEET_API_BASE}/${process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID_ACHIEVEMENT}/values/${range}?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`;
   const { values: achievementValues } = await fetch(sheetAchievementApi).then(
     (res) => res.json()
   );
@@ -87,8 +91,8 @@ export async function getStaticProps() {
 
   return {
     props: {
-      projectsData: projectsData ? projectsData : null,
-      achievementData: achievementData ? achievementData : null
+      projectsData,
+      achievementData
     }
   };
 }
