fd6677f0aee23d5c70d2f063317eaaa137bbc127
barrystone <barrystone3183@gmail.com>
2022-12-26 20:23:41 +0800

    [feat] add note api of nextjs api funcitonality

---
+++ pages/api/notes.js
@@ -0,0 +1,11 @@
+import analyzeReadme from '../../utils/analyzeReadme';
+
+export default async function handler(req, res) {
+  const resDate = await fetch(
+    'https://raw.githubusercontent.com/barrystone/hackmd_public-notes/master/README.md'
+  );
+  const data = await resDate.text();
+  const analyzedData = await analyzeReadme(data);
+
+  res.status(200).json(analyzedData);
+}
